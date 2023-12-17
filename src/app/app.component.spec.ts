9a37497b8bae2c42c0f40ce63b29f92797fb8309
Coff <82292207+CoffMakesThings@users.noreply.github.com>
2023-12-17 09:17:45 +0100

    fix double attack issue

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { unitConfigs } from "./units.config";
+import { secondaryUnitConfigs, unitConfigs } from "./units.config";
 import { Unit } from "./unit";
 import { Upgrades } from "./app.types";
 
@@ -25,6 +25,7 @@ describe('AppComponent', () => {
 
   it('should calculate', () => {
     const units: Unit[] = unitConfigs.map(u => new Unit(u));
+    const secondaryUnits: Unit[] = secondaryUnitConfigs.map(u => new Unit(u));
     const hydralisk: Unit = units.find(u => u.config.name === 'Hydralisk')!;
     const marine: Unit = units.find(u => u.config.name === 'Marine')!;
     const marauder: Unit = units.find(u => u.config.name === 'Marauder')!;
@@ -40,6 +41,14 @@ describe('AppComponent', () => {
     const probe: Unit = units.find(u => u.config.name === 'Probe')!;
     const hydra: Unit = units.find(u => u.config.name === 'Hydralisk')!;
     const archon: Unit = units.find(u => u.config.name === 'Archon')!;
+    const banshee: Unit = units.find(u => u.config.name === 'Banshee')!;
+    const stalker: Unit = units.find(u => u.config.name === 'Stalker')!;
+    const tempestAA: Unit = secondaryUnits.find(u => u.config.name === 'Tempest')!;
+    const battleCruiser: Unit = units.find(u => u.config.name === 'Battlecruiser')!;
+    const corruptor: Unit = units.find(u => u.config.name === 'Corruptor')!;
+    const queen: Unit = units.find(u => u.config.name === 'Queen')!;
+    const queenAA: Unit = secondaryUnits.find(u => u.config.name === 'Queen')!;
+    const colossus: Unit = units.find(u => u.config.name === 'Colossus')!;
 
     const upgradesA: Upgrades = {
       armor: 0,
@@ -59,7 +68,7 @@ describe('AppComponent', () => {
       infernalPreigniter: false
     }
 
-    // Hydra on marine
+    // Hydra on Marine
     expect(hydralisk.getRawHits(marine)).toEqual(4);
     upgradesA.weapons = 1;
     expect(hydralisk.getUpgradedHits(marine, upgradesA, upgradesB)).toEqual(4);
@@ -71,7 +80,7 @@ describe('AppComponent', () => {
     expect(hydralisk.getUpgradedHits(marine, upgradesA, upgradesB)).toEqual(4);
     upgradesA.weapons = 0;
 
-    // Marauder on roach
+    // Marauder on Roach
     expect(marauder.getRawHits(roach)).toEqual(8);
     upgradesA.weapons = 1;
     expect(marauder.getUpgradedHits(roach, upgradesA, upgradesB)).toEqual(7)
@@ -81,7 +90,7 @@ describe('AppComponent', () => {
     expect(marauder.getUpgradedHits(roach, upgradesA, upgradesB)).toEqual(6)
     upgradesA.weapons = 0;
 
-    // Viking vs viking
+    // Viking vs Biking
     expect(vikingFighter.getRawHits(vikingFighter)).toEqual(5);
     upgradesA.weapons = 1;
     expect(vikingFighter.getUpgradedHits(vikingFighter, upgradesA, upgradesB)).toEqual(5);
@@ -91,7 +100,7 @@ describe('AppComponent', () => {
     expect(vikingFighter.getUpgradedHits(vikingFighter, upgradesA, upgradesB)).toEqual(4);
     upgradesA.weapons = 0;
 
-    // Hellion vs scv
+    // Hellion vs SCV
     expect(hellion.getRawHits(scv)).toEqual(4);
     upgradesA.weapons = 1;
     expect(hellion.getUpgradedHits(scv, upgradesA, upgradesB)).toEqual(3);
@@ -108,7 +117,7 @@ describe('AppComponent', () => {
     upgradesA.infernalPreigniter = false;
     upgradesA.weapons = 0;
 
-    // Baneling vs drone
+    // Baneling vs Drone
     expect(baneling.getRawHits(drone)).toEqual(2);
     upgradesA.weapons = 1;
     expect(baneling.getUpgradedHits(drone, upgradesA, upgradesB)).toEqual(2);
@@ -118,28 +127,28 @@ describe('AppComponent', () => {
     expect(baneling.getUpgradedHits(drone, upgradesA, upgradesB)).toEqual(1);
     upgradesA.weapons = 0;
 
-    // Baneling vs zergling
+    // Baneling vs Zergling
     expect(baneling.getRawHits(zergling)).toEqual(1);
     upgradesB.armor = 1;
     expect(baneling.getUpgradedHits(zergling, upgradesA, upgradesB)).toEqual(2);
     upgradesB.armor = 0;
 
-    // Marine vs zealot
+    // Marine vs Zealot
     expect(marine.getRawHits(zealot)).toEqual(29);
 
-    // Zealot vs marine
+    // Zealot vs Marine
     expect(zealot.getRawHits(marine)).toEqual(3);
     upgradesB.combatShields = true;
     expect(zealot.getUpgradedHits(marine, upgradesA, upgradesB)).toEqual(4);
     upgradesB.combatShields = false;
 
-    // Marauder vs ultralisk
+    // Marauder vs Ultralisk
     expect(marauder.getRawHits(ultralisk)).toEqual(28);
     upgradesB.chitinousPlating = true;
     expect(marauder.getUpgradedHits(ultralisk, upgradesA, upgradesB)).toEqual(32);
     upgradesB.chitinousPlating = false;
 
-    // Roach vs probe
+    // Roach vs Probe
     expect(roach.getRawHits(probe)).toEqual(3);
     upgradesA.weapons = 1;
     expect(roach.getUpgradedHits(probe, upgradesA, upgradesB)).toEqual(3);
@@ -147,14 +156,44 @@ describe('AppComponent', () => {
     expect(roach.getUpgradedHits(probe, upgradesA, upgradesB)).toEqual(2);
     upgradesA.weapons = 0;
 
-    // Hydra vs archon
+    // Hydra vs Archon
     expect(hydra.getRawHits(archon)).toEqual(30);
     upgradesA.weapons = 1;
     expect(hydra.getUpgradedHits(archon, upgradesA, upgradesB)).toEqual(28);
     upgradesA.weapons = 0;
     upgradesB.shields = 1;
     expect(hydra.getUpgradedHits(archon, upgradesA, upgradesB)).toEqual(33);
-    // expect(roach.getUpgradedHits(probe, upgradesA, upgradesB)).toEqual(2);
-    // upgradesA.weapons = 0;
+    upgradesB.shields = 0;
+
+    // Banshee vs Stalker
+    expect(banshee.getRawHits(stalker)).toEqual(7);
+    upgradesA.weapons = 1;
+    expect(banshee.getUpgradedHits(stalker, upgradesA, upgradesB)).toEqual(7);
+    upgradesA.weapons = 2;
+    expect(banshee.getUpgradedHits(stalker, upgradesA, upgradesB)).toEqual(6);
+    upgradesA.weapons = 0;
+
+    // Tempest vs Battlecruiser
+    expect(tempestAA.getRawHits(battleCruiser)).toEqual(12);
+
+    // Corruptor vs Battlecruiser
+    expect(corruptor.getRawHits(battleCruiser)).toEqual(33);
+
+    // Archon vs Queen
+    expect(archon.getRawHits(queen)).toEqual(6);
+
+    // Queen vs Marine
+    expect(queen.getRawHits(marine)).toEqual(6);
+
+    // Queen vs Banshee
+    expect(queenAA.getRawHits(banshee)).toEqual(16);
+
+    // Colossus vs marine
+    expect(colossus.getRawHits(marine)).toEqual(2);
+    upgradesB.combatShields = true;
+    upgradesB.armor = 2;
+    expect(colossus.getUpgradedHits(marine, upgradesA, upgradesB)).toEqual(3);
+    upgradesB.combatShields = false;
+    upgradesB.armor = 0;
   });
 });
